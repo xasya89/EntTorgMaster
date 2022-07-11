63481425a801abb481c8990a06509c0cf3dfad38
Administrator <admin@example.com>
2022-07-11 14:43:37 +0300

    Новое меню и toolbar

---
+++ EntTorgMaster/Models/NavMenuItem.cs
@@ -0,0 +1,10 @@
+﻿namespace EntTorgMaster.Models
+{
+    public class NavMenuItem
+    {
+        public string Title { get; set; }
+        public string ImgSrc { get; set; }
+        public string Href { get; set; }
+        public bool isActive { get; set; }
+    }
+}
